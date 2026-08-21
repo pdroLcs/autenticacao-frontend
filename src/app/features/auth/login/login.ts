0e6f6970acea4ec8fcbc22b3d97a17c6493663fb
Pedro Lucas <plfm3k@gmail.com>
2026-08-21 13:35:06 -0400

    feat: componentiza o botão

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { Input } from "../../../shared/components/input/input";
+import { Button } from "../../../shared/components/button/button";
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, Input],
+  imports: [RouterLink, Input, Button],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
---
+++ src/app/shared/components/button/button.ts
@@ -0,0 +1,13 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.css',
+})
+export class Button {
+  type = input('submit');
+  label = input('');
+  class = input('');
+}
