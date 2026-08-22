b2427ba2eb6261c1e36c016a5a7eafe463197b6f
Pedro Lucas <plfm3k@gmail.com>
2026-08-22 11:37:18 -0400

    feat: implementa login com a API

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RegisterRequest } from '../../features/auth/models/register-request.model';
 import { Observable } from 'rxjs';
+import { LoginRequest } from '../../features/auth/models/login-request.model';
+import { LoginResponse } from '../../features/auth/models/login-response.model copy';
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +16,13 @@ export class Auth {
   register = (data: RegisterRequest): Observable<void> => {
     return this.http.post<void>(
       `${this.apiUrl}/register`,
+      data
+    );
+  }
+
+  login = (data: LoginRequest): Observable<LoginResponse> => {
+    return this.http.post<LoginResponse>(
+      `${this.apiUrl}/login`,
       data
     );
   }
--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { Input } from "../../../shared/components/input/input";
 import { Button } from "../../../shared/components/button/button";
+import { Auth } from '../../../core/services/auth';
+import { FormBuilder, NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, Input, Button],
+  imports: [RouterLink, Input, Button, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  private authService = inject(Auth);
+  private formBuilder = inject(NonNullableFormBuilder);
+
+  loginForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email, Validators.maxLength(255)]],
+    password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(255)]]
+  });
+
+  login = () => {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const data = this.loginForm.getRawValue();
+
+    this.authService.login(data).subscribe({
+      next: (response) => {
+        console.log('Login realizado');
+        console.log(response);
+      },
+      error: (error) => {
+        console.log('Erro ao fazer login', error);
+      }
+    })
+  }
+
+  get emailError(): string {
+    const email = this.loginForm.controls.email;
+
+    if (email.hasError('email') && email.touched) {
+      return 'E-mail inválido';
+    }
+    if (email.hasError('required') && email.touched) {
+      return 'E-mail é obrigatório';
+    }
+    return '';
+  }
+
+  get passwordError(): string {
+    const password = this.loginForm.controls.password;
+
+    if (password.hasError('required') && password.touched) {
+      return 'Senha é obrigatória';
+    }
+    return '';
+  }
+}
