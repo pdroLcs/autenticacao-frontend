96e5e7a636b56886a856a64449f82f050102473e
Pedro Lucas <plfm3k@gmail.com>
2026-08-21 23:34:55 -0400

    feat: adiciona mensagens de erro

--- src/app/shared/components/input/input.ts
+++ src/app/shared/components/input/input.ts
@@ -1,15 +1,49 @@
-import { Component, input } from '@angular/core';
+import { Component, forwardRef, input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl } from '@angular/forms';
+import { Error } from '../error/error';
 
 @Component({
   selector: 'app-input',
-  imports: [],
+  imports: [Error],
   templateUrl: './input.html',
   styleUrl: './input.css',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => Input),
+      multi: true
+    }
+  ]
 })
-export class Input {
+export class Input implements ControlValueAccessor {
+
   label = input('');
   type = input('text');
   name = input('');
   id = input('');
   placeholder = input('');
+  error = input('');
+
+  value = '';
+
+  onChange = (value: string) => {};
+  onTouched = () => {};
+
+  writeValue(value: string): void {
+    this.value = value ?? '';
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  onInput = (event: Event): void => {
+    const input = event.target as HTMLInputElement;
+    this.value = input.value;
+    this.onChange(this.value);
+  }
 }
