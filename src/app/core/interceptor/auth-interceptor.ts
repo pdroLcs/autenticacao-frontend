8de33330a40382494654c04376af0eec3e826ea8
Pedro Lucas <plfm3k@gmail.com>
2026-08-23 15:24:01 -0400

    feat: implementa refresh automatico quando token expira

--- src/app/core/interceptor/auth-interceptor.ts
+++ src/app/core/interceptor/auth-interceptor.ts
@@ -1,12 +1,15 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Token } from '../services/token';
+import { Auth } from '../services/auth';
+import { catchError, switchMap, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   const tokenService = inject(Token);
+  const authService = inject(Auth);
 
-  if (req.url.includes('/auth/login') || req.url.includes('/auth/register') || req.url.includes('/auth/refresh')) {
+  if (req.url.includes('/auth/login') || req.url.includes('/auth/register') || req.url.includes('/auth/refresh') || req.url.includes('/auth/logout')) {
     return next(req);
   }
 
@@ -20,5 +23,21 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     });
   }
 
-  return next(req);
+  return next(req).pipe(catchError((error: HttpErrorResponse) => {
+    if (error.status !== 401) {
+      return throwError(() => error);
+    }
+
+    return authService.refresh().pipe(switchMap(() => {
+      const newAccessToken = tokenService.getAccessToken();
+
+      const retryRequest = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${newAccessToken}`
+        }
+      });
+
+      return next(retryRequest);
+    }))
+  }));
 };
